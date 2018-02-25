0f864488fb95ec8e1c34e824f8124d70215b1bd2
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2018-02-25 18:27:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue Places Autocomplete
+
+Full source code for tutorial on using Google Places Autocomplete. The tutorial is posted on Medium.
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input ref="autocomplete" 
+        placeholder="Search" 
+        class="search-location"
+        onfocus="value = ''" 
+        type="text" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  mounted() {
+    this.autocomplete = new google.maps.places.Autocomplete(
+      (this.$refs.autocomplete),
+      {types: ['geocode']}
+    );
+
+    this.autocomplete.addListener('place_changed', () => {
+      let place = this.autocomplete.getPlace();
+      let ac = place.address_components;
+      let lat = place.geometry.location.lat();
+      let lon = place.geometry.location.lng();
+      let city = ac[0]["short_name"];
+
+      console.log(`The user picked ${city} with the coordinates ${lat}, ${lon}`);
+    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*, *::after, *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+body {
+  background-color: #dcdde1;
+  color: #2f3640;
+  padding: 3rem;
+}
+
+.search-location {
+  display: block;
+  width: 60vw;
+  margin: 0 auto;
+  margin-top: 5vw;
+  font-size: 20px;
+  font-weight: 400;
+  outline: none;
+  height: 30px;
+  line-height: 30px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border-radius: 10px;
 }
 </style>
